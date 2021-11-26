7d49f75cab7c1435b6b76238edf663b28c9e1150
Shruthi venkatesh <76960510+Shruthivenkatesh-05@users.noreply.github.com>
2021-11-26 19:19:44 +0530

    Assignment -6

---
+++ test.js
@@ -0,0 +1,81 @@
+Person1 = {
+    FirstName:"John ",
+    LastName :"Deo ",
+    Address : "Sherlock Holmes 221B Baker Street New York, NY 10001"
+};
+Person2 = {
+    FirstName:"Jack",
+    LastName :"Smith",
+    Address : "House no.45 second floor, 5th cross Banaswadi, Bangalore, KA 560043."
+};
+Person3 = {
+    FirstName:"Marry",
+    LastName :"Jones",
+    Address : "Cecilia Chapman 711-2880 Nulla St.Mankato Mississippi 965221"
+};
+
+function Display_FirstName1()
+{ 
+    console.log("First Name :"+ Person1.FirstName);
+    
+}
+function  Display_LastName1()
+{ 
+    console.log("Last Name :"+ Person1.LastName);
+    
+}
+function  Display_Address1()
+{ 
+    console.log("Address:"+ Person1.Address);
+    
+}
+function  Display_Properties1(Person)
+{ 
+    console.log("First Name :"+ Person1.FirstName);
+    console.log("Last Name :"+ Person1.LastName);
+    console.log("Address:"+ Person1.Address);
+}
+function Display_FirstName2()
+{ 
+    console.log("First Name :"+ Person2.FirstName);
+    
+}
+function  Display_LastName2()
+{ 
+    console.log("Last Name :"+ Person2.LastName);
+    
+}
+function  Display_Address2()
+{ 
+    console.log("Address:"+ Person2.Address);
+    
+}
+function  Display_Properties2()
+{ 
+    console.log("First Name :"+ Person2.FirstName);
+    console.log("Last Name :"+ Person2.LastName);
+    console.log("Address:"+ Person2.Address);
+}
+
+function Display_FirstName3()
+{ 
+    console.log("First Name :"+ Person3.FirstName);
+    
+}
+function  Display_LastName3()
+{ 
+    console.log("Last Name :"+ Person3.LastName);
+    
+}
+function  Display_Address3()
+{ 
+    console.log("Address:"+ Person3.Address);
+    
+}
+function  Display_Properties3(Person)
+{ 
+    console.log("First Name :"+ Person3.FirstName);
+    console.log("Last Name :"+ Person3.LastName);
+    console.log("Address:"+ Person3.Address);
+}
+
